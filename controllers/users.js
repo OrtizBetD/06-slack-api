b2895ae1bb7a173e7b0ebb9c3bea8017b80aaf6e
Orphe650 <davortiz@gmail.com>
2020-02-27 06:38:02 +0100

    encrypt part

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 // Require
 const Users = require("../models/users");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 // Routes
 router.post("/signup", (req, res) => {
@@ -10,6 +11,8 @@ router.post("/signup", (req, res) => {
       if (number != 0) {
         res.send("Email already exists");
       } else {
+        let encrypted = bcrypt.hashSync(req.body.password, 10);
+        req.body.password = encrypted;
         Users.create(req.body).then(user => {
           res.send(user);
         });
