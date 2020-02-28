fd08373b600db359d49f04fe024fe341f059431a
Orphe650 <davortiz@gmail.com>
2020-02-28 05:46:56 +0100

    reference part

--- models/users.js
+++ models/users.js
@@ -1,18 +1,19 @@
 // Require
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 // Model
-module.exports = mongoose.model('users', {
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	}
-})
+module.exports = mongoose.model("users", {
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
