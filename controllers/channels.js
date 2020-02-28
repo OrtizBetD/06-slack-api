909ba1cde3e3250a062885909cf8db65068c5a64
Orphe650 <davortiz@gmail.com>
2020-02-28 10:23:00 +0100

    callback in channels and messages functions was added

--- controllers/channels.js
+++ controllers/channels.js
@@ -17,15 +17,22 @@ router.post("/", (req, res) => {
     .catch(err => res.send("not authorized"));
 });
 router.get("/", (req, res) => {
-  Channels.find(req.query)
-    .then(channels => {
-      let token = req.headers.authorization.split(" ")[1];
-      let channel_info = jwt.verify(token, process.env.SECRET);
-      if (channel_info) {
-        res.send(channels);
-      }
-    })
-    .catch(err => res.send("not authorized"));
+  let token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET, (err, token_data) => {
+    console.log(token_data);
+    if (token_data) {
+      Channels.find(req.query)
+        .populate("user", "-password")
+        .then(channels => {
+          /*let token = req.headers.authorization.split(" ")[1];
+	      let message_info = jwt.verify(token, process.env.SECRET);*/
+          res.send(channels);
+        })
+        .catch(err => res.send(err));
+    } else {
+      res.send({ channel: "not authorized" });
+    }
+  });
 });
 
 // Export
--- controllers/messages.js
+++ controllers/messages.js
@@ -21,17 +21,34 @@ router.post("/", (req, res) => {
     .catch(err => res.send(err));
 });
 router.get("/", (req, res) => {
-  Messages.find(req.query)
+  let token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET, (err, token_data) => {
+    console.log(token_data);
+    if (token_data) {
+      Messages.find(req.query)
+        .populate("user", "-password")
+        .then(messages => {
+          /*let token = req.headers.authorization.split(" ")[1];
+	      let message_info = jwt.verify(token, process.env.SECRET);*/
+          res.send(messages);
+        })
+        .catch(err => res.send(err));
+    } else {
+      res.send({ message: "not authorized" });
+    }
+  });
+});
+
+/*Messages.find(req.query)
     .populate("user", "-password")
     .then(messages => {
-      let token = req.headers.authorization.split(" ")[1];
-      let message_info = jwt.verify(token, process.env.SECRET);
-      if (message_info) {
+      /*let token = req.headers.authorization.split(" ")[1];
+      let message_info = jwt.verify(token, process.env.SECRET);*/
+/*  if (message_info) {
         res.send(messages);
       }
     })
-    .catch(err => res.send("not authorized"));
-});
+    .catch(err => res.send("not authorized"));*/
 
 // Export
 module.exports = router;
