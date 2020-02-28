adcd98acb13a582bf252e1a156b0321058792760
Orphe650 <davortiz@gmail.com>
2020-02-28 07:33:30 +0100

    bearer token

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,11 +1,19 @@
 // Require
 const Messages = require("../models/messages");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Routes
 router.post("/", (req, res) => {
   Messages.create(req.body)
     .then(message => {
+      let token = req.headers.authorization.split(" ")[1];
+      console.log("token", token);
+      let user = jwt.verify(token, process.env.SECRET);
+      //  console.log("user", user);
+      message.user = user._id;
+      //  console.log("user", message.user);
       res.send(message);
     })
     .catch(err => res.send(err));
--- controllers/users.js
+++ controllers/users.js
@@ -17,6 +17,7 @@ router.post("/signup", (req, res) => {
         Users.create(req.body).then(user => {
           let plain_user = user.toObject();
           let token = jwt.sign(plain_user, process.env.SECRET);
+          console.log(token);
           res.send(token).catch(err => console.log(err));
         });
       }
@@ -32,6 +33,7 @@ router.post("/login", (req, res) => {
       if (match) {
         let plain_user = user.toObject();
         let token = jwt.sign(plain_user, process.env.SECRET);
+        console.log(token);
         res.send(token).catch(err => console.log(err));
       } else {
         res.send("invalid password");
