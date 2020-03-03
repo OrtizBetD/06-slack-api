665bfd8a31c884f0a17b3136e0ea422c8d9f83ae
Orphe650 <davortiz@gmail.com>
2020-03-03 09:04:44 +0100

    upload

--- controllers/messages.js
+++ controllers/messages.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
+require("dotenv").config();
 //SETTING STORAGE type
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -13,31 +14,68 @@ const path = require("path");
 
 const cloudinary = require("cloudinary");
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 // Routes
 // upload.single("file"),
 //define token
 //let token = req.headers.authorization.split(" ")[1];
-router.post("/", (req, res) => {
+router.post("/", upload.single("file"), (req, res) => {
   //console.log("file>>", req.file);
   let token = req.headers.authorization.split(" ")[1];
-  let data = jwt.verify(token, process.env.SECRET);
-  Users.findById(data._id).then(user => {
-    if (user.password == data.password) {
-      req.body.user = user._id;
-      Messages.create(req.body)
-        .then(message => {
-          console.log("token_mes", token);
-          let user = jwt.verify(token, process.env.SECRET);
-          //  console.log("user", user);
-          message.user = user._id;
-          //  console.log("user", message.user);
-          Messages.findById(message._id)
-            .populate("user", "-password")
-            .then(messages => {
-              res.send(messages);
+  jwt.verify(token, process.env.SECRET, (err, token_data) => {
+    if (token_data) {
+      if (req.file) {
+        const dataUri = new DataUri();
+        let uri = dataUri.format(
+          path.extname(req.file.originalname).toString(),
+          req.file.buffer
+        ).content;
+
+        cloudinary.uploader.upload(uri).then(cloudinaryFile => {
+          req.body.file = cloudinaryFile.url;
+          Users.findById(token_data._id).then(user => {
+            if (user.password == token_data.password) {
+              req.body.user = user._id;
+              Messages.create(req.body).then(message => {
+                console.log("token_mes", token);
+                let user = jwt.verify(token, process.env.SECRET);
+                //  console.log("user", user);
+                message.user = user._id;
+                //  console.log("user", message.user);
+                Messages.findById(message._id)
+                  .populate("user", "-password")
+                  .then(messages => {
+                    res.send(messages);
+                  })
+                  .catch(err => res.send(err));
+              });
+            }
+          });
+        });
+      } else {
+        Users.findById(token_data._id).then(user => {
+          if (user.password == token_data.password) {
+            req.body.user = user._id;
+            Messages.create(req.body).then(message => {
+              console.log("token_mes", token);
+              let user = jwt.verify(token, process.env.SECRET);
+              //  console.log("user", user);
+              message.user = user._id;
+              //  console.log("user", message.user);
+              Messages.findById(message._id)
+                .populate("user", "-password")
+                .then(messages => {
+                  res.send(messages);
+                })
+                .catch(err => res.send(err));
             });
-        })
-        .catch(err => res.send(err));
+          }
+        });
+      }
     } else {
       res.send("not authorized");
     }
